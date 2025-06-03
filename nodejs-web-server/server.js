d8db333467fd56774b2ccb5871fcce3dbd3fb4f2
Wariman <wariman.dev@gmail.com>
2025-06-03 22:55:48 +0700

    Add Route For Page Not Found

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -4,27 +4,27 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
 
-    const { method } = request;
+    const { method, url } = request;
 
-    if (method === 'GET') {
-        response.end('<h1>Ini adalah Method GET!</h1>');
-    }
+    // if (method === 'GET') {
+    //     response.end('<h1>Ini adalah Method GET!</h1>');
+    // }
 
-    if (method === 'POST') {
+    // if (method === 'POST') {
         
-        let body = [];
+    //     let body = [];
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk);
+    //     });
 
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}</h1>`);
-        });
+    //     request.on('end', () => {
+    //         body = Buffer.concat(body).toString();
+    //         const { name } = JSON.parse(body);
+    //         response.end(`<h1>Hai, ${name}</h1>`);
+    //     });
         
-    }
+    // }
 
     // if (method === 'PUT') {
     //     response.end('<h1>Ini adalah Method PUT!</h1>');
@@ -33,6 +33,19 @@ const requestListener = (request, response) => {
     // if (method === 'DELETE') {
     //     response.end('<h1>Ini adalah Method DELETE!</h1>');
     // }
+
+
+    if (url === '/') {
+        response.end('<h1>Selamat datang di halaman utama!</h1>');
+        
+    } else if (url === '/about') {
+        response.end('<h1>Selamat datang di halaman about!</h1>');
+
+    } else {
+        response.statusCode = 404;
+        response.end('<h1>Halaman tidak ditemukan!</h1>');
+        
+    }
 
 };
 
