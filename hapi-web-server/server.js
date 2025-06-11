d437fa623748398472bd1e788246bdd1cfa2ab8a
Wariman <wariman.dev@gmail.com>
2025-06-11 23:31:58 +0700

    Http Server With Hapi

--- hapi-web-server/server.js
+++ hapi-web-server/server.js
@@ -1,1 +1,38 @@
-console.log('Halo, kita akan belajar membuat server menggunakan Hapi');
+// console.log('Halo, kita akan belajar membuat server menggunakan Hapi');
+
+// Contoh Membuat HTTP Server tanpa routes
+// const Hapi = require('@hapi/hapi');
+
+// const init = async () => {
+//     const server = Hapi.server({
+//         port: 5000,
+//         host: 'localhost',
+//     });
+
+//     await server.start();
+//     console.log(`Server berjalan pada ${server.info.uri}`);
+// };
+
+// init();
+
+
+// Contoh Membuat HTTP Server dengan routes dan Hapi framework
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+
+    // Register routes
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
+
+
